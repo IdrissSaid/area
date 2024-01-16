4f0501e94a9dc4fa23329d7a5d9779a52dfaf62b
idriss <idriss.said@epitech.eu>
2024-01-16 16:13:59 +0400

    adsq

---
+++ server/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+export default async function middleware(request)
+{
+    const res = NextResponse.next()
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*')
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    return res;
+}
