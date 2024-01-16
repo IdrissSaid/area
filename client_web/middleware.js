8163514cdce341508ae7059ac52f701c0b6d71a5
idriss <idriss.said@epitech.eu>
2024-01-16 11:05:47 +0400

    url

--- client_web/middleware.js
+++ client_web/middleware.js
@@ -2,12 +2,15 @@ import { NextResponse } from 'next/server'
 
 export default async function middleware(request)
 {
+    const url = request.nextUrl.clone()
     if (request.nextUrl.pathname.startsWith("/_next")) return NextResponse.next();
     const token = request.cookies.get("session");
     if (request.nextUrl.pathname.startsWith("/dashboard") && !token?.value) {
-        return NextResponse.redirect("http://localhost:8081/login");
+        url.pathname = "/login"
+        return NextResponse.redirect(url);
     } else if ((request.nextUrl.pathname == "/login" || request.nextUrl.pathname == "/signUp") && token) {
-        return NextResponse.redirect("http://localhost:8081/dashboard");
+        url.pathname = "/dashboard"
+        return NextResponse.redirect(url);
     }
     return NextResponse.next();
 }
