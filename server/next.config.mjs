b0c836de65051fbd9d66360a639ac18aeec895a2
idriss <idriss.said@epitech.eu>
2024-01-16 16:07:44 +0400

    adsq

--- server/next.config.mjs
+++ server/next.config.mjs
@@ -1,16 +1,23 @@
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: " https://area-front-hvs1.vercel.app" },
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ];
-    }
-  };
-export default nextConfig;
+import cors from 'cors';
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  },
+  async configureServer(server) {
+    server.use(cors({
+      origin: 'https://area-front-hvs1.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    }));
+  }
+};
