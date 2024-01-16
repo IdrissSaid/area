edd8f09d73ca4075dc781866ae2fe9b9d75ab14a
idriss <idriss.said@epitech.eu>
2024-01-16 16:44:36 +0400

    git pushgit push

--- client_web/middleware.js
+++ client_web/middleware.js
@@ -4,13 +4,6 @@ export default async function middleware(request)
 {
     const url = request.nextUrl.clone()
     const res = NextResponse.next()
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', "https://area-front-hvs1.vercel.app")
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
     if (request.nextUrl.pathname.startsWith("/_next")) return res;
     const token = request.cookies.get("session");
     if (url.pathname === "/dashboard" && !token?.value) {
