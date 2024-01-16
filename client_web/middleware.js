6146e088a378cb481e982a1d1eefe692d156bc98
idriss <idriss.said@epitech.eu>
2024-01-16 16:12:46 +0400

    adsq

--- client_web/middleware.js
+++ client_web/middleware.js
@@ -3,7 +3,15 @@ import { NextResponse } from 'next/server'
 export default async function middleware(request)
 {
     const url = request.nextUrl.clone()
-    if (request.nextUrl.pathname.startsWith("/_next")) return NextResponse.next();
+    const res = NextResponse.next()
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*')
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (request.nextUrl.pathname.startsWith("/_next")) return res;
     const token = request.cookies.get("session");
     if (url.pathname === "/dashboard" && !token?.value) {
         url.pathname = "/login"
@@ -12,5 +20,5 @@ export default async function middleware(request)
         url.pathname = "/dashboard"
         return NextResponse.redirect(url);
     }
-    return NextResponse.next();
+    return res;
 }
--- client_web/next.config.js
+++ client_web/next.config.js
@@ -1,24 +1,4 @@
-// next.config.js
-import cors from 'cors';
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ];
-  },
-  async configureServer(server) {
-    server.use(cors({
-      origin: 'https://area-front-hvs1.vercel.app',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    }));
-  }
-};
+module.exports = nextConfig
