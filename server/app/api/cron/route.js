f051fb7bb1a5df0ec0f3c9622b34460fa67072e6
idriss <idriss.said@epitech.eu>
2024-01-16 12:26:17 +0400

    add ten minutes to cron

--- server/app/api/cron/route.js
+++ server/app/api/cron/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(req) {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return res.status(401).end('Unauthorized');
-    }
+export async function GET() {
     return NextResponse.json({ ok: true });
 }
